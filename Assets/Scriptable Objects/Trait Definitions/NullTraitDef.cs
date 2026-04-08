9bc98f45c9a6022eef917176e814f4cd5bec5bb3
cosmonaut2048 <cosmonaut2048@proton.me>
2026-04-08 17:05:17 +0300

    Новое: - NullTraitDef -- черта-пустышка. - При наведении курсора на ячейку описания черты появляется tooltip.

---
+++ Assets/Scriptable Objects/Trait Definitions/NullTraitDef.cs
@@ -0,0 +1,11 @@
+﻿using Content;
+using UnityEngine;
+
+namespace Scriptable_Objects.Trait_Definitions
+{
+    [CreateAssetMenu(fileName = "NullTraitDef", menuName = "Scriptable Objects/Traits/NullTraitDef")]
+    public class NullTraitDef : TraitDef
+    {
+        
+    }
+}
--- Assets/Scripts/UI/HiringScreen/HireController.cs
+++ Assets/Scripts/UI/HiringScreen/HireController.cs
@@ -24,14 +24,28 @@ namespace UI.HiringScreen
             }
         }
         
+        // Спрайт портрета и имя работника.
         private VisualElement _workerPortrait;
         private Label _workerName;
         
+        // Контейнеры навыков.
         private VisualElement _skillPatienceCellsContainer;
         private VisualElement _skillSocialCellsContainer;
         private VisualElement _skillIntellectualCellsContainer;
         private VisualElement _skillPhysicalCellsContainer;
         
+        // Название черты, информация о черте.
+        private Label _trait1Name;
+        private VisualElement _trait1InfoCell;
+        private VisualElement _trait1InfoHover;
+        private Label _trait1Description;
+        
+        private Label _trait2Name;
+        private VisualElement _trait2InfoCell;
+        private VisualElement _trait2InfoHover;
+        private Label _trait2Description;
+        
+        // Ячейки навыков (получаем из контейнеров).
         private List<VisualElement> _patienceCells;
         private List<VisualElement> _socialCells;
         private List<VisualElement> _intellectualCells;
@@ -41,38 +55,65 @@ namespace UI.HiringScreen
         {
             var root = GetComponent<UIDocument>().rootVisualElement;
             
-            // Queue
+            // Queue:
+            // Спрайт портрета и имя работника.
             _workerPortrait = root.Q<VisualElement>("worker_portrait");
             _workerName = root.Q<Label>("worker_card_name");
+            // Навыки.
             _skillPatienceCellsContainer = root.Q<VisualElement>("Skill_Patience_Cells_Container");
             _skillSocialCellsContainer = root.Q<VisualElement>("Skill_Social_Cells_Container");
             _skillIntellectualCellsContainer = root.Q<VisualElement>("Skill_Intellectual_Cells_Container");
             _skillPhysicalCellsContainer = root.Q<VisualElement>("Skill_Physical_Cells_Container");
+            // Черты.
+            _trait1Name = root.Q<Label>("trait_1_name");
+            _trait1InfoCell = root.Q<VisualElement>("trait_1_info_cell");
+            _trait1InfoHover =  root.Q<VisualElement>("trait_1_info_hover");
+            _trait1Description = root.Q<Label>("trait_1_description");
             
-            // Кэшируем ячейки навыков
+            _trait2Name = root.Q<Label>("trait_2_name");
+            _trait2InfoCell = root.Q<VisualElement>("trait_2_info_cell");
+            _trait2InfoHover =  root.Q<VisualElement>("trait_2_info_hover");
+            _trait2Description = root.Q<Label>("trait_2_description");
+            
+            // Кэшируем ячейки навыков.
             CacheSkillCells();
             
             // Инициализируем из сериализованного поля.
             _worker = worker;
             OnWorkerChanged();
+            
+            // Скрываем элементы.
+            _trait1InfoHover.style.display = DisplayStyle.None;
+            _trait2InfoHover.style.display = DisplayStyle.None;
+            
+            _trait1InfoCell.RegisterCallback<MouseEnterEvent>(evt => OnInfoCellEnter(_trait1InfoHover));
+            _trait1InfoHover.RegisterCallback<MouseOutEvent>(evt => OnInfoLeave(_trait1InfoHover));
+    
+            _trait2InfoCell.RegisterCallback<MouseEnterEvent>(evt => OnInfoCellEnter(_trait2InfoHover));
+            _trait2InfoHover.RegisterCallback<MouseOutEvent>(evt => OnInfoLeave(_trait2InfoHover));
+        }
+
+        private void OnInfoCellEnter(VisualElement targetHover)
+        {
+            targetHover.style.display = DisplayStyle.Flex;
+            Debug.Log("OnInfoCellEnter");
+        }
+
+        private void OnInfoLeave(VisualElement targetHover)
+        {
+            targetHover.style.display = DisplayStyle.None;
+            Debug.Log("OnInfoLeave");
         }
         
-        // Для отслеживания в инспекторе.
+        /// <summary>
+        /// Для отслеживания изменений в инспекторе.
+        /// </summary>
         private void OnValidate()
         {
             if (Application.isPlaying && worker != _worker)
             {
                 Worker = worker;
             }
-        }
-        
-        private void CacheSkillCells()
-        {
-            // Получаем ячейки из каждого контейнера.
-            _patienceCells = _skillPatienceCellsContainer?.Children().ToList() ?? new List<VisualElement>();
-            _socialCells = _skillSocialCellsContainer?.Children().ToList() ?? new List<VisualElement>();
-            _intellectualCells = _skillIntellectualCellsContainer?.Children().ToList() ?? new List<VisualElement>();
-            _physicalCells = _skillPhysicalCellsContainer?.Children().ToList() ?? new List<VisualElement>();
         }
         
         private void OnWorkerChanged()
@@ -89,6 +130,13 @@ namespace UI.HiringScreen
                 UpdateSkillCells(_socialCells, _worker.BaseSocial);
                 UpdateSkillCells(_intellectualCells, _worker.BaseIntellectual);
                 UpdateSkillCells(_physicalCells, _worker.BasePhysical);
+                
+                // Обновляем информацию о чертах.
+                _trait1Name.text = _worker.PersonalityTraits[0]?.traitName;
+                _trait1Description.text = _worker.PersonalityTraits[0]?.traitDescription;
+                
+                _trait2Name.text = _worker.PersonalityTraits[1]?.traitName;
+                _trait2Description.text = _worker.PersonalityTraits[1]?.traitDescription;
             }
             else
             {
@@ -97,6 +145,23 @@ namespace UI.HiringScreen
             }
         }
         
+        /// <summary>
+        /// Кеширование ячеек навыков из контейнеров.
+        /// </summary>
+        private void CacheSkillCells()
+        {
+            // Получаем ячейки из каждого контейнера.
+            _patienceCells = _skillPatienceCellsContainer?.Children().ToList() ?? new List<VisualElement>();
+            _socialCells = _skillSocialCellsContainer?.Children().ToList() ?? new List<VisualElement>();
+            _intellectualCells = _skillIntellectualCellsContainer?.Children().ToList() ?? new List<VisualElement>();
+            _physicalCells = _skillPhysicalCellsContainer?.Children().ToList() ?? new List<VisualElement>();
+        }
+
+        /// <summary>
+        /// Заполнение ячеек по уровню навыка.
+        /// </summary>
+        /// <param name="cells">Список ячеек.</param>
+        /// <param name="skillLevel">Уровень навыка.</param>
         private void UpdateSkillCells(List<VisualElement> cells, int skillLevel)
         {
             if (cells == null || cells.Count == 0) return;
@@ -123,6 +188,9 @@ namespace UI.HiringScreen
             }
         }
         
+        /// <summary>
+        /// Возвращение всех ячеек в пустое состояние.
+        /// </summary>
         private void ClearAllSkillCells()
         {
             ClearSkillCells(_patienceCells);
@@ -144,5 +212,6 @@ namespace UI.HiringScreen
                 }
             }
         }
+        
     }
 }
